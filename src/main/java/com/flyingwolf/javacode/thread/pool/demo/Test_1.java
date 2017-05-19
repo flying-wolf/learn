7e7530bafb8838948791ca9390446417c4c8e4e3
flying-wolf <sggw@163.com>
2017-05-19 18:21:13 +0800

    ThreadPoolExecutor

--- src/main/java/com/flyingwolf/javacode/thread/pool/demo/Test_1.java
+++ src/main/java/com/flyingwolf/javacode/thread/pool/demo/Test_1.java
@@ -1,11 +1,17 @@
 package com.flyingwolf.javacode.thread.pool.demo;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class Test_1 {
 
 	public static void main(String[] args) {
+		ThreadPoolExecutor tpe = new ThreadPoolExecutor(10, 10, 0L, 
+				TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 		for (int i = 0; i < 10; i++) {
 			Thread t = new Thread(new Runnable() {
@@ -14,9 +20,11 @@ public class Test_1 {
 					System.out.println(Thread.currentThread().getName());
 				}
 			});
-			executor.submit(t);
+			//executor.submit(t);
+			tpe.submit(t);
 		}
-		executor.shutdown();
+		//executor.shutdown();
+		tpe.shutdown();
 	}
 	
 	
